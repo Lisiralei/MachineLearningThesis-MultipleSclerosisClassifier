f87c8b93df9ea0173d08556f75acf1443e5c8193
Lisiralei <realallerrr1@gmail.com>
2023-06-12 00:53:46 +0500

    main changes for running the training

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+
 import data_manipulation
 from models import sclerosis_cnn
 
 
+def dataset_manipulation_snippet():
+    excel_file = 'D:\\Machine Learning Data\\MultipleSclerosisTest\\labels.xlsx'
+    dataset_path_for_label = 'D:\\Machine Learning Data\\Multiple Sclerosis pngs'
+
+    folder_labels = data_manipulation.excel_to_label(excel_file)
+    print('folder labels:', folder_labels)
+    data_manipulation.set_flag_for_all(dataset_path_for_label, flag_source=folder_labels, classes=('no', 'yes'))
+    data_manipulation.remove_empty_folders(dataset_path_for_label)
 
 
 if __name__ == '__main__':
-    #data_manipulation.cleanup('D:\\Machine Learning Data\\MultipleSclerosisTest')
+
+    #dataset_manipulation_snippet()
 
     dataset_path = 'D:\\Machine Learning Data\\MultipleSclerosisTest'
     print(sclerosis_cnn.run_cnn(dataset_path, epochs=5, batch_size=18, save_frequency=5, verbosity=2))
+
